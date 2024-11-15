20847d0d49d7adf629c5bd010d799bf69314aac1
suraj subedi <surajsubedi059@gmail.com>
2024-11-15 17:11:59 +0545

    feat: minor fixes

--- src/components/business-tracker/BusinessTracker.tsx
+++ src/components/business-tracker/BusinessTracker.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import Hero from "../home/Hero";
 import BusinessTrackerForm from "./BusinessTrackerForm";
@@ -8,6 +9,9 @@ const BusinessTracker = (props: Props) => {
   return (
     <div className="space-y-6 pb-10">
       <Hero />
+      <h1 className="custom-outline text-4xl text-center text-gray-800 font-extrabold uppercase">
+        WHY I JOINED PFA
+      </h1>
       <BusinessTrackerForm />
     </div>
   );
